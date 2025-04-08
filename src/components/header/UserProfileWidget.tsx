33a9612ae577c393c53c8ec9753cf21ea1d7824d
Cas Doorn <casdoorn.system@gmail.com>
2025-04-08 14:59:53 +0200

    Changed svg's and links

--- src/components/header/UserProfileWidget.tsx
+++ src/components/header/UserProfileWidget.tsx
@@ -52,7 +52,7 @@ export default function UserProfileWidget({ user, onClose }: UserProfileWidgetPr
           <DropdownItem
             onItemClick={onClose}
             tag="a"
-            href="/profile"
+            href="/admin/profile"
             className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
           >
             <svg
@@ -77,7 +77,46 @@ export default function UserProfileWidget({ user, onClose }: UserProfileWidgetPr
           <DropdownItem
             onItemClick={onClose}
             tag="a"
-            href="/profile"
+            href="https://cloud.appwrite.io/console/project-67efe659000a3f5b3ac5/overview/platforms"
+            className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
+          >
+            <svg 
+              className="fill-gray-500 group-hover:fill-gray-700 dark:fill-gray-400 dark:group-hover:fill-gray-300"
+              width="24" 
+              height="24" 
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg" 
+            >
+              <g transform="translate(0.5, 0.5)">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M23 15.8V20.6H10.5C6.8 20.6 3.6 18.6 1.9 15.6C1.6 15.1 1.4 14.7 1.2 14.2C0.8 13.3 0.6 12.2 0.5 11.2V9.8C0.5 9.6 0.5 9.3 0.6 9.1C0.6 8.6 0.7 8.1 0.9 7.7C2 3.5 5.9 0.5 10.5 0.5C15.1 0.5 19 3.5 20.1 7.7H14.9C14 6.2 12.3 5.3 10.5 5.3C8.7 5.3 7 6.2 6.1 7.7C5.8 8.1 5.6 8.6 5.4 9.1C5.3 9.6 5.2 10.1 5.2 10.5C5.2 12 5.9 13.4 6.9 14.4C7.8 15.3 9.1 15.8 10.5 15.8H23Z"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                />
+                <path
+                  d="M23 9.1V14.4H14.1C15.1 13.4 15.8 12 15.8 10.5C15.8 10.1 15.7 9.6 15.6 9.1H23Z"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                />
+              </g>
+            </svg>
+            Appwrite Console
+          </DropdownItem>
+        </li>
+        <li>
+          <DropdownItem
+            onItemClick={onClose}
+            tag="a"
+            href="#"
             className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
           >
             <svg
@@ -95,32 +134,7 @@ export default function UserProfileWidget({ user, onClose }: UserProfileWidgetPr
                 fill=""
               />
             </svg>
-            Account settings
-          </DropdownItem>
-        </li>
-        <li>
-          <DropdownItem
-            onItemClick={onClose}
-            tag="a"
-            href="/profile"
-            className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
-          >
-            <svg
-              className="fill-gray-500 group-hover:fill-gray-700 dark:fill-gray-400 dark:group-hover:fill-gray-300"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M3.5 12C3.5 7.30558 7.30558 3.5 12 3.5C16.6944 3.5 20.5 7.30558 20.5 12C20.5 16.6944 16.6944 20.5 12 20.5C7.30558 20.5 3.5 16.6944 3.5 12ZM12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM10.9991 7.52507C10.9991 8.07736 11.4468 8.52507 11.9991 8.52507H12.0001C12.5524 8.52507 13.0001 8.07736 13.0001 7.52507C13.0001 6.97279 12.5524 6.52507 12.0001 6.52507H11.9991C11.4468 6.52507 10.9991 6.97279 10.9991 7.52507ZM12.0001 17.3714C11.5859 17.3714 11.2501 17.0356 11.2501 16.6214V10.9449C11.2501 10.5307 11.5859 10.1949 12.0001 10.1949C12.4143 10.1949 12.7501 10.5307 12.7501 10.9449V16.6214C12.7501 17.0356 12.4143 17.3714 12.0001 17.3714Z"
-                fill=""
-              />
-            </svg>
-            Support
+            Documentation
           </DropdownItem>
         </li>
       </ul>
